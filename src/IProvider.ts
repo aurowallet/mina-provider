2362b4085c905684697fafc3d3dfbe0929c9ca3b
lvshaoping007 <1244849816@qq.com>
2023-03-15 22:53:17 +0800

    fix: reset event listener

--- src/IProvider.ts
+++ src/IProvider.ts
@@ -9,6 +9,7 @@ import {
 
 export interface IMinaProvider {
   request(args: RequestArguments): Promise<unknown>
+  isConnected(): boolean
   sendTransaction(args: SendTransactionArgs): Promise<SendTransactionResult>
   sendLegacyPayment(args: SendLegacyPaymentArgs): Promise<BroadcastTransactionResult>
   sendLegacyStakeDelegation(args: SendLegacyStakeDelegationArgs): Promise<BroadcastTransactionResult>
@@ -19,4 +20,21 @@ export interface IMinaProvider {
 
   signFields(args: SignFieldsArguments): Promise<SignedFieldsData>
   verifyFields(args: VerifyFieldsArguments): Promise<boolean>
+
+  // Events
+  on(eventName: 'connect', listener: ConnectListener): this
+  on(eventName: 'disconnect', listener: ConnectListener): this
+  on(eventName: 'chainChanged', listener: ChainChangedListener): this
+  on(eventName: 'accountsChanged', listener: AccountsChangedListener): this
+
+  removeListener(eventName: 'disconnect', listener: ConnectListener): this
+  removeListener(eventName: 'connect', listener: ConnectListener): this
+  removeListener(
+    eventName: 'chainChanged',
+    listener: ChainChangedListener
+  ): this
+  removeListener(
+    eventName: 'accountsChanged',
+    listener: AccountsChangedListener
+  ): this
 }
--- src/provider.ts
+++ src/provider.ts
@@ -24,15 +24,21 @@ export default class MinaProvider extends EventEmitter implements IMinaProvider{
   private readonly channel: MessageChannel
   private readonly messenger: Messenger
   public readonly isAuro: boolean = true
+  private connectedFlag: boolean
   
   constructor() {
     super()
     this.channel = new MessageChannel('webhook')
     this.messenger = new Messenger(this.channel)
+    this.initEvents()
   }
 
   public request({method, params}: RequestArguments): Promise<any> {
     return this.messenger.send(method, params)
+  }
+
+  public isConnected(): boolean {
+    return this.connectedFlag
   }
 
   public async sendTransaction(args: SendTransactionArgs): Promise<SendTransactionResult>  {
@@ -73,6 +79,38 @@ export default class MinaProvider extends EventEmitter implements IMinaProvider{
 
   public async verifyFields(args: VerifyFieldsArguments): Promise<boolean> {
     return this.request({method: DAppActions.mina_verifyFields, params: args})
+  }
+  private initEvents() {
+    this.channel.on('connect', this.onConnect.bind(this))
+    this.channel.on('disconnect', this.onDisconnect.bind(this))
+    this.channel.on('chainChanged', this.onChainChanged.bind(this))
+    this.channel.on('networkChanged', this.onNetworkChanged.bind(this))
+    this.channel.on(
+      'accountsChanged',
+      this.emitAccountsChanged.bind(this)
+    )
+  }
+
+  private onConnect(): void {
+    this.connectedFlag = true
+    this.emit('connect')
+  }
+
+  private onDisconnect(error: ProviderError): void {
+    this.connectedFlag = false
+    this.emit('disconnect', error)
+  }
+
+  private onChainChanged(chainId: string): void {
+    this.emit('chainChanged', chainId)
+  }
+
+  private onNetworkChanged(network: string): void {
+    this.emit('networkChanged', network)
+  }
+
+  private emitAccountsChanged(accounts: string[]): void {
+    this.emit('accountsChanged', accounts)
   }
 }
 
