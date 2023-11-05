c401330f52ed812a28a43859bf4cd6cdef94621a
lvshaoping007 <1244849816@qq.com>
2023-11-05 21:30:29 +0800

    add error code

--- src/TSTypes.ts
+++ src/TSTypes.ts
@@ -1,7 +1,14 @@
 /**
  * code 4001 The request was rejected by the user
+ * code 4300 User disconnect, need connect first
+ * code -32005 Have Pending chain action
+ * code -32006 Not support chain
+ * code -32007 No wallet found
+ * code -32008 Verify failed
  * code -32602 The parameters were invalid
  * code -32603 Internal error
+ * code -32800 Unspecified error message
+ * code -32900 Origin dismatch
  */
 export interface ProviderError extends Error {
   message: string;
