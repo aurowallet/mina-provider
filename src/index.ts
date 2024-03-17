7e4b63cb306432872cd58e0b17e430394e2850d7
lvshaoping007 <1244849816@qq.com>
2024-03-17 19:54:10 +0800

    Add getSiteIcon export

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import { DAppActions } from "./constant";
 import MessageChannel from "./lib/messageChannel";
 import MinaProvider from "./provider";
+import { getSiteIcon } from "./utils";
 export default MinaProvider;
-export * from  "./TSTypes";
-export {
-  DAppActions,
-  MessageChannel
-};
+export * from "./TSTypes";
+
+export { DAppActions, MessageChannel, getSiteIcon };
